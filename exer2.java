339997bda92cf6ed96b39fa32c8bd36fb7508ee5
b4rb4r4-c3cim <barbara.cecim@gmail.com>
2023-01-19 20:57:56 -0300

    matrizes-java

---
+++ exer2.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+public class exer2 {
+    public static void main(String[] args){
+        int[][] numerosAleatorios = new int[4][4];
+
+        Random numeroRandom = new Random();
+
+        for(int i=0; i < numerosAleatorios.length; i++){
+            for(int j=0; j < numerosAleatorios[i].length; j++){
+                numerosAleatorios[i][j] = numeroRandom.nextInt(100); // nós escolhemos até qual numero podemos gerar aleatorios
+
+            }
+        }
+        
+        int maior = 0;
+        int linha = 0;
+        int coluna = 0;
+
+        
+        for(int i=0; i < numerosAleatorios.length; i++){
+            for(int j=0; j < numerosAleatorios[i].length; j++){
+                numerosAleatorios[i][j] = numeroRandom.nextInt(100); // nós escolhemos até qual numero podemos gerar aleatorios
+
+                if(numerosAleatorios[i][j] > maior){
+                    maior = numerosAleatorios[i][j];
+                    linha = i;
+                    coluna = j;
+                }
+            }
+        }
+         
+        for(int i=0; i < numerosAleatorios.length; i++){
+            for(int j=0; j < numerosAleatorios[i].length; j++){
+                System.out.print(numerosAleatorios[i][j] + " ");
+
+            }
+            System.out.println();
+    }
+
+    System.out.println("Maior valor = " + maior);
+    System.out.println("Linha = " + linha);
+    System.out.println("Coluna = " + coluna);
+}
+}
